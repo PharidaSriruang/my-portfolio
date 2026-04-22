e15b20fead3ce5deb0d905211f4b65574f7763bb
parida <s64122519031@ssru.ac.th>
2026-04-22 22:19:10 +0700

    first portfolio

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", fontFamily: "Arial", marginTop: "50px" }}>
+      <h1>สวัสดี ผมชื่อ [ชื่อคุณ]</h1>
+      <p>Front-End Developer (Beginner)</p>
+
+      <h2>Skills</h2>
+      <p>HTML / CSS / JavaScript / React</p>
+
+      <h2>Projects</h2>
+      <ul style={{ listStyle: "none" }}>
+        <li>Todo App</li>
+        <li>Weather App</li>
+        <li>Portfolio Website</li>
+      </ul>
+
+      <h2>Contact</h2>
+      <p>GitHub: https://github.com/yourname</p>
+    </div>
+  );
+}
+
+export default App;
